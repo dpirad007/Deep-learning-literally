f213be29a114c045e398d35b1239ae3d0931fe6e
dpirad007 <dionpinto000@gmail.com>
2022-11-25 22:08:50 +0530

    autograd

---
+++ autograd/ion.py
@@ -0,0 +1,110 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import graphviz
+
+
+class Atom:
+
+    def __init__(self, data, children=(), _op="", label=""):
+        self.data = data
+        self._prev = set(children)
+        self._op = _op
+        self.label = label
+        self.grad = 0.0
+        self._backward = lambda: None
+
+    def __repr__(self):
+        return f"Atom(data={self.data})"
+
+    def __add__(self, other):
+        o = Atom(self.data + other.data, (self, other), "+")
+
+        def backward():
+            print("__add__ backward invoked")
+            print(f"Self: {self}, other: {other}, o: {o}")
+            self.grad += 1.0 * o.grad
+            other.grad += 1.0 * o.grad
+            print(
+                f"self.grad: {self.grad}, other.grad: {other.grad}, o.grad: {o.grad}")
+
+        o._backward = backward
+
+        return o
+
+    def __mul__(self, other):
+        o = Atom(self.data * other.data, (self, other), "*")
+
+        def backward():
+            print("__mul__ backward invoked")
+            print(f"Self: {self}, other: {other}, o: {o}")
+
+            self.grad += other.data * o.grad
+            other.grad += self.data * o.grad
+            print(
+                f"self.grad: {self.grad}, other.grad: {other.grad}, o.grad: {o.grad}")
+
+        o._backward = backward
+
+        return o
+
+    def tanh(self):
+        n = self.data
+        t = (math.exp(2*n)-1)/(math.exp(2*n)+1)
+        o = Atom(t, (self,), "tanh")
+
+        def backward():
+            print("tanh backward invoked")
+            print(f"Self: {self}, o: {o}")
+            self.grad += (1 - t**2) * o.grad
+            print(
+                f"self.grad: {self.grad}, o.grad: {o.grad}")
+
+        o._backward = backward
+        return o
+
+    def backward(self):
+
+        self.grad = 1.0
+
+        topo = []
+        vis = set()
+
+        def sorttopo(node):
+            if node not in vis:
+                vis.add(node)
+                for child in node._prev:
+                    sorttopo(child)
+                topo.append(node)
+        sorttopo(self)
+        for node in reversed(topo):
+            node._backward()
+
+
+def traverse(root):
+    nodes, edges = set(), set()
+
+    def build(node):
+        if node not in nodes:
+            nodes.add(node)
+        for child in node._prev:
+            edges.add((child, node))
+            build(child)
+    build(root)
+    return nodes, edges
+
+
+def graph(root):
+
+    dot = graphviz.Digraph(graph_attr={"rankdir": "LR"})
+
+    nodes, edges = traverse(root)
+
+    for node in nodes:
+        uid = str(id(node))
+        dot.node(name=uid, label="%s | data %.4f | grad %.4f" %
+                 (node.label, node.data, node.grad), shape="record")
+
+    for e1, e2 in edges:
+        dot.edge(str(id(e1)), str(id(e2)))
+    return dot
---
+++ autograd/main.py
@@ -0,0 +1,44 @@
+from ion import *
+
+# simple example
+
+# a = Atom(1.0, label="a")
+# b = Atom(7.0, label="b")
+# f = Atom(4.0, label="f")
+# e = Atom(3.0, label="e")
+
+
+# c = a+b
+# c.label = "c"
+
+# d = c*e
+# d.label = "d"
+
+# L = d+f
+# L.label = "L"
+# graph(L).view()
+
+# neuron example
+x1 = Atom(2.0, label="x1")
+w1 = Atom(-3.0, label="w1")
+x2 = Atom(0.0, label="x2")
+w2 = Atom(1.0, label="w2")
+b = Atom(6.8813735870195432, label='b')
+
+x1w1 = x1*w1
+x1w1.label = "x1w1"
+x2w2 = x2*w2
+x2w2.label = "x2w2"
+
+x1w1x2w2 = x1w1+x2w2
+x1w1x2w2.label = "x1w1x2w2"
+
+n = x1w1x2w2 + b
+n.label = "n"
+
+o = n.tanh()
+o.label = "tanh"
+
+o.backward()
+
+graph(o).view()
